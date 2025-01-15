4313305cf5bfa46baf01d39bc84e1b29a2152d62
Oleksii <oleksii.ruzhytskiy@loopme.com>
2025-01-15 11:59:29 +0200

    added gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,14 @@ import tailwindcss from 'tailwindcss'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/crypto_tracker/',
   plugins: [react()],
   css: {
     postcss: {
       plugins: [tailwindcss()],
     },
+  },
+  build: {
+    outDir: 'dist', // Ensure this matches your gh-pages deployment folder
   },
 })
